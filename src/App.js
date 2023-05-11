1f76a665e47c8fce6dba1dd452a1aa58486f1d6a
Kurein <kaueh1234@gmail.com>
2023-05-11 16:08:41 -0300

    Test commit

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ function App() {
   return (
     <MainContainer>
       <HamburgerMenu />
+      {useGlobalState("language")[0] === "pt" ? (
+        <h1>Ola Mundo</h1>
+      ) : (
+        <h1>Hello World</h1>
+      )}
     </MainContainer>
   );
 }
